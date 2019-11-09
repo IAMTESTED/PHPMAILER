b0dcdec26d3746aeea8d28b586491eb37513b7a2
TESTED12 <41592851+TESTED12@users.noreply.github.com>
2019-11-09 16:53:45 +0530

    Update INEX.php

--- INEX.php
+++ INEX.php
@@ -11,7 +11,7 @@ try {
   $mail->Host='smtp.gmail.com;';
   $mail->SMTPAuth=true;
   $mail->Username='A.K.GUPTA.3216@gmail.com';
-  $mail->Password='AKG070797';
+  $mail->Password='#';
   $mail->SMTPSecure='tls';
   $mail->Port=587;
 
